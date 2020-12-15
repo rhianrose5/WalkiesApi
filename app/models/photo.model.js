535aeaf11854678715d7643c0a87f34e5b93be02
DESKTOP-BN5JHCD\rhian <rhian.rose@landmark.co.uk>
2020-12-15 11:35:48 +0000

    Added photoId to photo model

--- app/models/photo.model.js
+++ app/models/photo.model.js
@@ -4,6 +4,7 @@ module.exports = mongoose => {
         mongoose.Schema({
             walkId: Number,
             walkName: String,
+            photoId: Number,
             photo: String
         })
     );
